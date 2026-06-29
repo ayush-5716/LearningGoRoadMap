08c68fee78c2af61456e217d157699e318f9a8a4
ayush.nandkumar <ayush.nandkumar@volantetech.com>
2026-06-29 14:37:55 +0530

    Day-1

--- code/HelloWorld.go
+++ code/HelloWorld.go
@@ -3,5 +3,14 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("how are my amigos ")
+
+	if _, sub, sum := operations(10, 5); sub > 100 && sum > 20 {
+		fmt.Println("The answer is big ", sub)
+	} else {
+		fmt.Println("The answer is smaller than hundred: ", sum)
+	}
+}
+
+func operations(x int, y int) (int, int, int) {
+	return x * y, x - y, x + y
 }
